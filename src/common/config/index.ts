384f5170015cd338b195869267881acdc594dd0b
su <recallsufuture@gmail.com>
2024-03-17 02:28:49 +0800

    feat: remove s3 helper

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -4,9 +4,10 @@ export interface ServerConfig {
   port: number;
 }
 
-export interface Baichuan2Config {
-  baseUrl?: string;
-  modelPath?: string;
+export interface LLMInferServerConfig {
+  hosts: string[];
+  controlApiPort: number;
+  inferApiPort: number;
 }
 
 export interface CredentialEncryptConfig {
@@ -14,20 +15,10 @@ export interface CredentialEncryptConfig {
   privateKey: string;
 }
 
-export interface S3Config {
-  accessKeyId: string;
-  secretAccessKey: string;
-  endpoint: string;
-  region: string;
-  modelBucketName: string;
-  assetsBucketName: string;
-  assetsBucketPublicUrl: string;
-}
-
 export interface Config {
   server: ServerConfig;
+  llmInferServer: LLMInferServerConfig;
   credentialEncrypt: CredentialEncryptConfig;
-  s3: S3Config;
 }
 
 const port = readConfig('server.port', 3001);
@@ -36,6 +27,10 @@ export const config: Config = {
   server: {
     port,
   },
+  llmInferServer: {
+    hosts: readConfig('llmInferServer.hosts', []),
+    controlApiPort: readConfig('llmInferServer.controlApiPort', 3000),
+    inferApiPort: readConfig('llmInferServer.inferApiPort', 8000),
+  },
   credentialEncrypt: readConfig('credentialEncrypt', {}),
-  s3: readConfig('s3', {}),
 };
